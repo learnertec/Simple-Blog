859fc8d18eecffcf061292d35ada2f8a2970f4b6
Your Name <you@example.com>
2018-07-17 10:45:04 +0530

    login session and logout method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,76 @@
 class UsersController < ApplicationController
   
-  def index
-  @users = User.paginate(page: params[:page], per_page: 5)
-  end
+  before_action :set_user, only: [:edit, :update, :show]
+  
 
+def index
 
-  def new
-  	@user = User.new
-  end
+@users = User.paginate(page: params[:page], per_page: 5)
 
-  def create
-     @user = User.new(user_params)
-     if @user.save
-       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-       redirect_to articles_path
-     else
-      render 'new'
-     end
-  end
+end
 
-  def edit
-   @user = User.find(params[:id])
-  end
+def new
 
-  def update
-   @user = User.find(params[:id])
-   if @user.update(user_params)
-     flash[:success]= "Your account was updated sucessfully"
-     redirect_to articles_path
-   else
-    render 'edit'
-   end
-  end
+@user = User.new
 
+end
 
-  def show
-   @user = User.find(params[:id])
-   @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-  end
+def create
 
-  private
+@user = User.new(user_params)
 
-  def user_params
-   params.require(:user).permit(:username, :email, :password)
-  end
+if @user.save
+
+flash[:success] = "Welcome to the alpha blog #{@user.username}"
+
+redirect_to articles_path
+
+else
+
+render 'new'
+
+end
+
+end
+
+def edit
+
+end
+
+def update
+
+if @user.update(user_params)
+
+flash[:success] = "Your account was updated successfully"
+
+redirect_to articles_path
+
+else
+
+render 'edit'
+
+end
+
+end
+
+def show
+
+@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+
+end
+
+private
+
+def user_params
+
+params.require(:user).permit(:username, :email, :password)
+
+end
+
+def set_user
+
+@user = User.find(params[:id])
+
+end 
 
 end
