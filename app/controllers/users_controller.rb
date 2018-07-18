0b961c7804915a3ed28caa47dcbd2de7ab3dde50
Your Name <you@example.com>
2018-07-18 10:38:26 +0530

    resadd triction to controlller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:edit, :update, :show]
-  
+before_action :set_user, only: [:edit, :update, :show]
+before_action :require_same_user, only: [:edit,:update]
 
 def index
 
@@ -72,5 +72,12 @@ def set_user
 @user = User.find(params[:id])
 
 end 
+
+def require_same_user
+ if current_user != @user
+ flash[:danger] = "You can only edit your own account"
+ redirect_to root_path
+end
+end
 
 end
